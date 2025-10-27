022a2ca9b4fbea9a3c2904d4216cbb47b1f676eb
LAPT-CSH-13 <cshtd.computacion@gmail.com>
2025-10-27 08:28:58 -0600

    Creación de botones y background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <header className= " bg-fuchsia-950 text-white">
-      <div className="container mx-auto flex justify-between items-center p-10">
+    <><header className=" bg-fuchsia-950 text-white mb-16" >
+      <div className="container mx-auto flex justify-end items-center p-10">
+        <button className="text-white py-2 px-4 rounded-lg font-bold">
+            Conexiones
+        </button>
+        <button className="bg-white text-black py-2 px-4 rounded-lg font-bold">
+            Aspiraciones
+          </button>
       </div>
     </header>
+
+    <div 
+        className="bg-cover bg-center w-full min-h-screen" 
+        style={{ backgroundImage: "url('/img/back_ground.jpg')" }}
+      >
+
+        <button className="bg-white text-black py-2 px-4 rounded-lg font-bold"></button>
+        
+      </div>
+    </>
   );
 }
-
-
