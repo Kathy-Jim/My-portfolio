6da4ca9c9d89b5d36534e84d47227192372d87c4
Kath Jimenez <kjimenezmartinez7@gmail.com>
2025-11-01 00:06:51 -0600

    Mejora leve del sitio, creación de secciones

---
+++ src/app/Aspiraciones/page.tsx
@@ -0,0 +1,11 @@
+export default function AspiracionesPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-4xl font-bold text-fuchsia-950 mb-6">Aspiraciones laborales</h1>
+      <p className="text-lg text-gray-700">
+        Página en proceso
+      </p>
+      
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { link } from "fs";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,23 +6,27 @@ export default function Home() {
     <>
       <header className="bg-fuchsia-950 text-white mb-16">
         <div className="container mx-auto flex justify-between items-center p-4 sm:p-6 lg:p-10">
-            <span className="text-xl font-semibold">Mi portafolio</span>
-            <div className="flex space-x-4">
-                <button className="text-white py-2 px-4 rounded-lg font-bold">
-                    Conexiones
-                </button>
-                <button className="**bg-white text-black** py-2 px-4 rounded-lg font-bold">
-                    Aspiraciones
-                </button>
-            </div>
+          <span className="text-xl font-semibold">Mi portafolio</span>
+
+          <div className="flex space-x-4">
+            <button className="text-white py-2 px-4 rounded-lg font-bold">
+              Conexiones
+            </button>
+
+            <Link href="/Aspiraciones" passHref>
+              <span className="bg-white text-black py-2 px-4 rounded-lg font-bold cursor-pointer inline-block">
+                Aspiraciones
+              </span>
+            </Link>
+
+          </div>
         </div>
       </header>
 
       <div
-        className="bg-cover bg-center w-full min-h-screen flex items-end **justify-end** p-8" 
+        className="bg-cover bg-center w-full min-h-screen flex items-end **justify-end** p-8"
         style={{ backgroundImage: "url('/img/back_ground.jpg')" }}
       >
-
         <div
           className="bg-black/70 text-white p-4 rounded-xl font-normal text-xl"
         >
@@ -32,12 +36,32 @@ export default function Home() {
           <br />
           <span>criminología</span>
         </div>
-
       </div>
 
-      <body>
-        
-      </body>
+
+      <div className="**bg-fuchsia-300** **text-black**">
+        <div className="container mx-auto p-8">
+          <br />
+          <br />
+          <h2 className="text-black text-3xl font-bold mb-6">Sobre mí</h2>
+          <div className="flex justify-right mb-9">
+             <Image className="rounded-3xl" src="/img/img01.png" alt="profile_photo" width={350} height={0}/>
+          </div>
+        </div>
+      </div>
+
+    <div className="container mx-auto p-8">
+      <h2 className="text-black text-3xl font-bold mb-6">Educación</h2>
+      <p className="text-lg text-gray-700 mb-4">
+        <Image className="rounded-3xl mb-4" src="/img/logo_hilasal.jpg" alt="education_photo" width={300} height={0}/>
+      </p>
+      <p className="text-lg text-gray-700">
+        <Image className="rounded-3xl mb-4" src="/img/logo_sanjose.png" alt="education_photo" width={300} height={0}/>     
+      </p>
+      <p className="text-lg text-gray-700">
+        <Image className="rounded-3xl mb-4" src="/img/logo_kodigo.jpg" alt="" width={300} height={0}/>
+      </p>
+      </div>
     </>
   );
 }
